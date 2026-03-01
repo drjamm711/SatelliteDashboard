0d91869d3778713b02a8401859d857ae315209d2
drjamm711 <tyguy711@gmail.com>
2026-03-01 14:37:34 -0700

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { applyPolyfills, defineCustomElements } from '@astrouxds/astro-web-components/loader'
 import App from './App'
 import './index.css'
@@ -10,8 +10,8 @@ applyPolyfills().then(() => defineCustomElements())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
